f13d0a601e7872680393368bda8beaf7011b7a58
Luân Nguyễn <luannguyen@Luans-MacBook-Air.local>
2023-11-29 10:00:30 +0700

    update

--- complete/components/Home/Recommendations.jsx
+++ complete/components/Home/Recommendations.jsx
@@ -1,42 +1,57 @@
-import { FlatList, StyleSheet, ActivityIndicator, TouchableOpacity, View } from "react-native";
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import { SIZES } from "../../constants/theme";
-import fetchRecommendations from "../../hook/fetchRecommendations";
-import HorizontalShimmer from "../Shimmers/HorizontalShimmer";
-import ReusableTile from "../Reusable/ReusableTile";
+import {
+  FlatList,
+  StyleSheet,
+  ActivityIndicator,
+  TouchableOpacity,
+  View,
+} from "react-native"
+import React from "react"
+import { useNavigation } from "@react-navigation/native"
+import { SIZES } from "../../constants/theme"
+// import fetchRecommendations from "../../hook/fetchRecommendations"s
+import data from "./data.json"
+import HorizontalShimmer from "../Shimmers/HorizontalShimmer"
+import ReusableTile from "../Reusable/ReusableTile"
 
 const Recommendations = () => {
-  const navigation = useNavigation();
-  const {recommendations, isLoading, error, refetch} = fetchRecommendations(1);
- 
+  const navigation = useNavigation()
+  // const { recommendations, isLoading, error, refetch } = fetchRecommendations(1)
 
-if(isLoading){
-  return <HorizontalShimmer horizontal={true} width={'100%'} height={100} radius={12} paddingTop={15}/>
-}
+  // if (isLoading) {
+  //   return (
+  //     <HorizontalShimmer
+  //       horizontal={true}
+  //       width={"100%"}
+  //       height={100}
+  //       radius={12}
+  //       paddingTop={15}
+  //     />
+  //   )
+  // }
 
   return (
     <View style={styles.container}>
-      
-
-    <FlatList 
-    data={recommendations}
-    horizontal
-    keyExtractor={(item)=> item._id}
-    contentContainerStyle={{columnGap: SIZES.medium}}
-    showsHorizontalScrollIndicator={false}
-    renderItem={({item}) => (
-        <ReusableTile item={item} onPress={()=> navigation.navigate('PlaceDetails', item._id)}/>
-    )}
-    />
+      <FlatList
+        data={data.recommendations}
+        horizontal
+        keyExtractor={(item) => item._id}
+        contentContainerStyle={{ columnGap: SIZES.medium }}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <ReusableTile
+            item={item}
+            onPress={() => navigation.navigate("PlaceDetails", item._id)}
+          />
+        )}
+      />
     </View>
-  );
-};
+  )
+}
 
-export default Recommendations;
+export default Recommendations
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
   },
-});
+})
--- complete/screens/home/Home.jsx
+++ complete/screens/home/Home.jsx
@@ -1,29 +1,27 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React from "react";
-import reusable from "../../components/Reusable/reusable.style";
-import { ReusableText, HeightSpacer, Recommendations } from "../../components";
-import Places from '../../components/Home/Places'
-import { COLORS, SIZES, TEXT } from "../../constants/theme";
-import { AntDesign, Feather } from "@expo/vector-icons";
-import styles from "./home.style";
-import BestHotels from "../../components/Home/BestHotels";
-import checkUser from "../../hook/checkUser";
-
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import React from "react"
+import reusable from "../../components/Reusable/reusable.style"
+import { ReusableText, HeightSpacer, Recommendations } from "../../components"
+// import Places from "../../components/Home/Places"
+import { COLORS, SIZES, TEXT } from "../../constants/theme"
+import { AntDesign, Feather } from "@expo/vector-icons"
+import styles from "./home.style"
+import BestHotels from "../../components/Home/BestHotels"
+import checkUser from "../../hook/checkUser"
 
 const Home = ({ navigation }) => {
-  
-  const {userLogin,userData ,isLoading, time}  = checkUser()
+  const { userLogin, userData, isLoading, time } = checkUser()
   return (
     <View style={reusable.container}>
-       <HeightSpacer height={50}/>
+      <HeightSpacer height={50} />
       <View>
         <View style={reusable.rowWithSpace("space-between")}>
           <ReusableText
-            text= {userLogin? `${time} ${userData.username}`: `${time} User!`}
+            text={userLogin ? `${time} ${userData.username}` : `${time} User!`}
             family={"regular"}
             size={TEXT.large}
             color={COLORS.black}
-          /> 
+          />
 
           <TouchableOpacity
             style={styles.box}
@@ -35,42 +33,38 @@ const Home = ({ navigation }) => {
 
         <HeightSpacer height={SIZES.xLarge} />
 
-        <ReusableText
+        {/* <ReusableText
           text={"Places"}
           family={"medium"}
           size={TEXT.large}
           color={COLORS.black}
         />
         
-        <Places />
+        <Places /> */}
 
-        <HeightSpacer height={35}/>
+        {/* <HeightSpacer height={35} /> */}
 
-        <View
-        style={[reusable.rowWithSpace("space-between")]}
-      >
-        <ReusableText
-          text={"Recommendations"}
-          family={"medium"}
-          size={TEXT.large}
-          color={COLORS.black}
-        />
+        <View style={[reusable.rowWithSpace("space-between")]}>
+          <ReusableText
+            text={"Recommendations"}
+            family={"medium"}
+            size={TEXT.large}
+            color={COLORS.black}
+          />
 
-        <TouchableOpacity onPress={() => navigation.navigate("Recommended")}>
-          <Feather name="list" size={20} />
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity onPress={() => navigation.navigate("Recommended")}>
+            <Feather name="list" size={20} />
+          </TouchableOpacity>
+        </View>
 
         <Recommendations />
 
-        <HeightSpacer height={30}/>
+        <HeightSpacer height={30} />
 
         <BestHotels />
-
-        
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
